928e0c24c8973f03343bab19492d869ec8d3097c
TEACHER\ICT02_22 <admin@admin.com>
2024-09-11 11:34:46 +0900

    첫번째 커밋 - 스프링 기본 설정

---
+++ src/main/java/edu/ict/ex/SpringBootHjsIctExampleApplication.java
@@ -0,0 +1,13 @@
+package edu.ict.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHjsIctExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHjsIctExampleApplication.class, args);
+	}
+
+}
