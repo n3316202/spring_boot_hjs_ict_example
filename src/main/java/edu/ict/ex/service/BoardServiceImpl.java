92406620aef3b653f0dc8accca4c4a74f388ca65
TEACHER\ICT02_22 <admin@admin.com>
2024-09-30 10:03:45 +0900

    commit

--- src/main/java/edu/ict/ex/service/BoardServiceImpl.java
+++ src/main/java/edu/ict/ex/service/BoardServiceImpl.java
@@ -3,6 +3,8 @@ package edu.ict.ex.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import edu.ict.ex.mapper.BoardMapper;
@@ -22,6 +24,7 @@ public class BoardServiceImpl implements BoardService {
 	public List<BoardVO> getList() {
 		log.info("getList() ..");
 		
+			
 		return boardMapper.getList();
 	}
 
